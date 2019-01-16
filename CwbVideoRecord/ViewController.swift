ddb7b17374dd808e5075de18167cf8e8f53aee57
陈武兵 <wubing@wubing.local>
2019-01-16 18:53:39 +0800

    Initial Commit

---
+++ CwbVideoRecord/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CwbVideoRecord
+//
+//  Created by  武兵 on 2019/1/16.
+//  Copyright © 2019年 TheGiant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
