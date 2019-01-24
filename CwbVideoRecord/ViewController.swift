78e19e6aa08f90ea73285dddd0928c1aa50b209a
TheGiant <wubing@wubing.local>
2019-01-25 00:35:23 +0800

    更新

--- CwbVideoRecord/ViewController.swift
+++ CwbVideoRecord/ViewController.swift
@@ -3,10 +3,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var imgView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.privacyPermission(type: PrivacyPermissionType.camera)
     }
     
     @IBAction func buttonAction(_ sender: UIButton) {
